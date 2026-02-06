babbdf1d7cebf9bde745799fd84564eb61868ffe
Эльвира <kimelya1208@gmail.com>
2026-02-06 12:50:29 +0300

    Добавлены базовые классы Driver и Order

--- Drivers/Program.cs
+++ Drivers/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+namespace DriverMatching
+{
+    public class Driver
+    {
+        public int Id { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Driver(int id, int x, int y)
+        {
+            Id = id;
+            X = x;
+            Y = y;
+        }
+    }
+
+    public class Order
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Order(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
